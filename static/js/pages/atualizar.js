603cf92c8e66ba2b0afa1a6c5f2685081da97efb
Danilo2505 <77727610+Danilo2505@users.noreply.github.com>
2025-10-01 12:58:27 -0300

    Telas excluir.html e atualizar.html
    Falta conexão com BD

---
+++ static/js/pages/atualizar.js
@@ -0,0 +1,42 @@
+const selectModoEdicao = document.querySelector("#select-ediciao");
+const selectorsSectionsModosEdicao = [
+  "#section-disciplina",
+  "#section-sala",
+  "#section-aluno",
+  "#section-nota",
+];
+
+function definirModoEdicao(modoSelecionado) {
+  // Coloca a classe escondido em cada seção de modo de edição
+  selectorsSectionsModosEdicao.forEach((modoEdicao) => {
+    document.querySelector(modoEdicao).classList.add("escondido");
+  });
+
+  // Retira a classe "escondido" do modo de edição selecionado
+  switch (modoSelecionado) {
+    // Modo de edição de disciplinas
+    case "disciplina":
+      document
+        .querySelector("#section-disciplina")
+        .classList.remove("escondido");
+      break;
+    // Modo de edição de salas
+    case "sala":
+      document.querySelector("#section-sala").classList.remove("escondido");
+      break;
+    // Modo de edição de alunos
+    case "aluno":
+      document.querySelector("#section-aluno").classList.remove("escondido");
+      break;
+    // Modo de edição de notas
+    case "nota":
+      document.querySelector("#section-nota").classList.remove("escondido");
+      break;
+  }
+}
+
+definirModoEdicao(selectModoEdicao.value);
+
+selectModoEdicao.addEventListener("change", function () {
+  definirModoEdicao(this.value);
+});
